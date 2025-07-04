42ff7216cd62b7a0433898b0a28008462f049a0d
Subh <subhaushsingh@gmail.com>
2025-07-04 13:07:14 +0530

    adding routing with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {BrowserRouter,Link,Route,Routes} from 'react-router-dom'
+import {Home,CreatePost} from './pages'
+import logo from './assets/ai-technology.svg';
+
 
 const App = () => {
   return (
-    <div className='bg-gray-500'>App</div>
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4  '>
+        <Link to='/'>
+          <img src={logo} alt="logo" className="w-15 object-contain"/>
+        </Link>
+        <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link>
+      </header>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/create-post' element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
