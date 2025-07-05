d64e3b40773aeee01abacb6da9479338a0147167
Subh <subhaushsingh@gmail.com>
2025-07-05 20:26:44 +0530

    createPost and handleSurpriseMe function

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -22,7 +22,7 @@ const FormField = ({
         <button
           type="button"
           onClick={handleSurpriseMe}
-          className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black"
+          className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black cursor-pointer"
         >
           Surprise me
         </button>
