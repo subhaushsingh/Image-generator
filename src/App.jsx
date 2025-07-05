683cf7b5137abbbaf106b66861f09f76bfe01945
Subh <subhaushsingh@gmail.com>
2025-07-05 17:23:28 +0530

    development of home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import {BrowserRouter,Link,Route,Routes} from 'react-router-dom'
 import {Home,CreatePost} from './pages'
-import logo from './assets/ai-technology.svg';
+import logo from './assets/logo.svg';
 
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4  '>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 mt-[10px] '>
         <Link to='/'>
-          <img src={logo} alt="logo" className="w-15 object-contain"/>
+          <img src={logo} alt="logo" className="w-15 h-10 object-contain"/>
         </Link>
         <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link>
       </header>
